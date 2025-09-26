1f28d0b729e603135650c2c82778871a0c21705a
matiuho <142462529+matiuho@users.noreply.github.com>
2025-09-26 10:38:08 -0300

    user y admin

--- TuCanchaFS/TuCancha/TuCanchaFS-main/TuCanchaFS-main/ecommerce/assets/js/menu.js
+++ TuCanchaFS/TuCancha/TuCanchaFS-main/TuCanchaFS-main/ecommerce/assets/js/menu.js
@@ -14,3 +14,5 @@ class MyHeader extends HTMLElement {
     }
 }
 customElements.define('my-header', MyHeader);
+
+
--- TuCanchaFS/TuCancha/TuCanchaFS-main/TuCanchaFS-main/ecommerce/assets/js/validar.js
+++ TuCanchaFS/TuCancha/TuCanchaFS-main/TuCanchaFS-main/ecommerce/assets/js/validar.js
@@ -1,29 +1,43 @@
-function validaAdmin() {
-    console.log('email : ' + document.getElementById('email').value);
-    console.log('password : ' + document.getElementById('password').value);
-    if (document.getElementById('email').value == 'admin' && document.getElementById('password').value == '12345') { 
-        document.location.href = '../admin/index.html'
-    } else { 
-        alert('Usuario no permitido!');
+
+const btnLogin = document.getElementById('btnLogin');
+btnLogin.addEventListener('click', (e)=> {
+    e.preventDefault();
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if(email === 'admin' && password === '12345') {        
+        document.location.href = '../admin/index.html';        
     }
-}
+
+    if (email === 'matias@duocuc.cl' && password === '12345') { 
+        document.location.href = '../ecommerce/index.html'            
+    }
+
+    else{ 
+        console.log("Usuario no permitido");
+    }
+
+});
+
+
 
 function addCart() { 
-    console.log('addCart');
-    contador = contador + 1;
-}
+     console.log('addCart');
+     contador = contador + 1;
+ }
 
 if (sessionStorage.getItem('carro') == null) {
-    sessionStorage.setItem('carro', '0');
-}
+     sessionStorage.setItem('carro', '0');
+ }
 
 function leerNumero() { 
-    var valor = sessionStorage.getItem('carro');
-    return valor 
-}
+     let valor = sessionStorage.getItem('carro');
+     return valor 
+ }
 
-function agregarNumero() {
-    var valor = sessionStorage.getItem('carro');
-    suma = parseInt(valor) + 1;
-    sessionStorage.setItem('carro', suma);
-}
+ function agregarNumero() {
+     let valor = sessionStorage.getItem('carro');
+     suma = parseInt(valor) + 1;
+     sessionStorage.setItem('carro', suma);
+ }
